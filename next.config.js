6ebc590864f9a44dbacb9a6e5f532d600297b9e9
SarKurd <sarbast2030@gmail.com>
2020-01-01 01:34:10 +0100

    PWA support
    fix favicon
    
    fix favicons

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
-const path = require('path');
+const withOffline = require('next-offline');
 
-module.exports = {
+module.exports = withOffline({
   env: {
     SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
+  },
+  workboxOpts: {
+    swDest: 'static/service-worker.js'
+  },
+  experimental: {
+    async rewrites() {
+      return [
+        {
+          source: '/service-worker.js',
+          destination: '/_next/static/service-worker.js'
+        }
+      ];
+    }
   }
-};
+});
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,17 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name="theme-color" content="#000000" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" type="image/png" sizes="192x192" href="/pwa.png" />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="192x192"
+            href="/pwa.png"
+          />
+        </Head>
         <body className="dark">
           <Main />
           <NextScript />
